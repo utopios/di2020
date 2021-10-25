158cd4bca89bc0e65ecf697b0ce8c458afe56bdd
ihab <ihab@utopios.net>
2021-10-25 16:45:00 +0200

    debut ihm

---
+++ CompteBancaire/Classes/IHM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace CompteBancaire.Classes
+{
+    public class IHM
+    {
+
+        Banque banque;
+        public IHM()
+        {
+            banque = new Banque();
+        }
+
+
+        void MenuPrinpal()
+        {
+
+        }
+        void MenuCreationCompte()
+        {
+
+        }
+
+        Compte ActionRechercherCompte()
+        {
+            Console.WriteLine("Merci de saisir le numero du compte : ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Compte compte = banque.RechercherCompte(numero);
+            return compte;
+        }
+
+        void ActionEffectuerDepot()
+        {
+            Compte compte = ActionRechercherCompte();
+            if(compte != null)
+            {
+                Console.WriteLine("Merci de saisir le montant de depot : ");
+                decimal montant = Convert.ToDecimal(Console.ReadLine());
+                if(compte.Depot(new Operation(montant)))
+                {
+                    Console.WriteLine("Depot effecuté");
+                }
+                else
+                {
+                    Console.WriteLine("Erreur depot");
+                }
+            }
+        }
+
+        void ActionEffectuerRetrait()
+        {
+
+        }
+
+    }
+}
