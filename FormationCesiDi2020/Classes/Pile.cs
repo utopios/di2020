c26467c1e7a73a4ccccc5f81d60d98a3308918b1
ihab <ihab@utopios.net>
2021-10-25 10:45:59 +0200

    correction pile

---
+++ FormationCesiDi2020/Classes/Pile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace FormationCesiDi2020.Classes
+{
+    public class Pile<T>
+    {
+        private T[] elements;
+        private int compteur;
+
+        public Pile(int taille)
+        {
+            elements = new T[taille];
+            compteur = 0;
+        }
+
+        public void Empiler(T element)
+        {
+            if(compteur < elements.Length - 1)
+            {
+                //elements[compteur] = element;
+                //compteur++;
+                elements[compteur++] = element;
+            }
+        }
+
+        public void Depiler()
+        {
+            if(elements.Length > 0)
+            {
+                elements[--compteur] = default(T);
+            }
+        }
+
+        public T GetElement(int place)
+        {
+            if(compteur > place && place >=0)
+            {
+                return elements[place];
+            }
+            return default(T);
+        }
+    }
+}
