c8edd0c8ebb90f5a4f6a9a0ab3710f3233fa010d
ihab <ihab@utopios.net>
2021-10-28 09:44:54 +0200

    debut correction compte bancaire

---
+++ correction-compte-bancaire/Controllers/CompteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompteBancaire.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace correction_compte_bancaire.Controllers
+{
+    public class CompteController : Controller
+    {
+        // GET: /<controller>/
+        //Page d'accueil qui affiche le formulaire de recherche
+        public IActionResult Index(int? compteId)
+        {
+            Compte compte = null;
+            if(compteId != null)
+            {
+                compte = Compte.GetCompteById((int)compteId);
+            }
+            return View(compte);
+        }
+    }
+}
---
+++ correction-compte-bancaire/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace correction_compte_bancaire.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
